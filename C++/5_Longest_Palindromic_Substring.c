4294d679a1fbecf6f7ebb452d5d26b1083f74d2f
viewer <2638480742@qq.com>
2017-02-24 17:07:30 +0800

    only need 6ms,but my python's solution need 1100+ms

---
+++ C++/5_Longest_Palindromic_Substring.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<iostream>
+using namespace std;
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        if (s.size() < 2)
+            return s;
+        int len = s.size(), max_left = 0, max_len = 1, left, right;
+        for (int start = 0; start < len && len - start > max_len / 2;) {
+            left = right = start;
+            while (right < len - 1 && s[right + 1] == s[right])
+                ++right;
+            start = right + 1;
+            while (right < len - 1 && left > 0 && s[right + 1] == s[left - 1]) {
+                ++right;
+                --left;
+            }
+            if (max_len < right - left + 1) {
+                max_left = left;
+                max_len = right - left + 1;
+            }
+        }
+        return s.substr(max_left, max_len);
+    }
+};
+
+int main(){
+    Solution example;
+    string s="babad";
+    cout<<example.longestPalindrome(s)<<endl;
+    
+}
